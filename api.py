ab2b0ce7a2b13a79912bc9d144a91a630906744f
Shaunak Badani <shaunak.badani@duke.edu>
2024-09-07 20:37:19 -0400

    Configure logging on the App

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import logging
 
 
 app = Flask(__name__)
+logging.basicConfig(filename = 'logs/info.log', level = logging.INFO)
+logging.basicConfig(filename = 'logs/debug.log', level = logging.DEBUG)
+logging.basicConfig(filename = 'logs/error.log', level = logging.ERROR)
 
 
 @app.route("/")
 def hello_world():
+    app.logger.info('Someone trying to access Hello World!')
     return "<p>Hello, world!</p>"
